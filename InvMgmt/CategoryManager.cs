13dbc0382a14452fe10964b49613c3b0dee8404e
Jaks01 <42505518+Jaks01@users.noreply.github.com>
2019-08-19 23:20:45 -0400

    added search bar and delete button ui

---
+++ InvMgmt/CategoryManager.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InvMgmt
+{
+    class CategoryManager
+    {
+        public ObservableCollection<CategoryViewModel> categories = new ObservableCollection<CategoryViewModel>();
+        public int currentCategoryIndex = 0;
+    }
+}
